b253af5be988fdb923688ae968345c6b5ed56b93
Eliyas Kassaye <eliyasson11@gmail.com>
2023-01-30 20:45:56 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      {/*1. Header */}
+      <h1>Todo List App</h1>
+
+      {/*2.Input (input and button)*/} 
+      <input
+        type="text"
+        placeholder='Add an item...'
+      />
+
+      {/*3. List of items (unordered list with list items)*/}
       
     </div>
   );
