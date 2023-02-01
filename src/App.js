e56d6d4e29812f2fee7575b9449c3b34986b39cb
Eliyas Kassaye <eliyasson11@gmail.com>
2023-02-01 10:56:35 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,27 @@
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  // state hook - 'useState
+  const [newItem, setNewItem] = useState("");
+  const [items, setItems] = useState([]);
+
+  // helper function
+  function addItem() {
+    if(!newItem) {
+      alert("Enter a player name.")
+    }
+
+    const item = {
+      id: Math.floor(Math.random() * 1000),
+      value: newItem
+    };
+
+    setItems(oldList => [...oldList, item]);
+    setNewItem("")
+    console.log(items);
+  }
+
   return (
     <div className="App">
       {/*1. Header */}
@@ -10,9 +31,17 @@ function App() {
       <input
         type="text"
         placeholder='Add an item...'
+        value={newItem}
+        onChange={e => setNewItem(e.target.value)}
       />
+      <button onClick={() => addItem()}>Add</button>
 
-      {/*3. List of items (unordered list with list items)*/}
+      {/*3. List of players (unordered list with list players)*/}
+      <ul>
+        <li>rolando</li>
+        <li>rolando</li>
+        <li>rolando</li>
+      </ul>
       
     </div>
   );
